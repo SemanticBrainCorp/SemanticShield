eabd08a7785a9f8d9c0ce96ca8ae6dd53fdd9399
albertsemantic <albert.r@semanticbrain.net>
2023-09-21 10:06:03 -0400

    update setup

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='shield',
+    name='SemanticShield',
     version='0.0.1',
     author='SemanticBrain',
     author_email='albert.r@semanticbrain.net',
     description='SemanticShield library',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/albertsemantic/shield',
+    url='https://github.com/SemanticBrainCorp/SemanticShield',
     project_urls = {
-        "Bug Tracker": "https://github.com/albertsemantic/shield/issues"
+        "Bug Tracker": "https://github.com/SemanticBrainCorp/SemanticShield/issues"
     },
-    license='proprietary',
+    license='MIT',
     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     install_requires=["openai==0.28.0","langchain==0.0.287","spacy==3.6.1","Faker==19.6.1","presidio-analyzer==2.2.33","exrex==0.11.0"],
 )
