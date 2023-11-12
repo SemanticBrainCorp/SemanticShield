757a919a195b9d6532f462c43078e96fdafb0ee8
albertsemantic <albert.r@semanticbrain.net>
2023-11-12 16:20:26 -0500

    pypi build

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker buildx build --platform linux/amd64 -t semanticshield:0.1.1 .
+docker buildx build --platform linux/amd64 -t semanticshield:0.1.4 .
 docker images | grep shield
--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='SemanticShield',
-    version='0.1.1',
+    version='0.1.4',
     author='SemanticBrain',
-    author_email='albert.r@semanticbrain.net',
+    author_email='info@semanticbrain.net',
     description='SemanticShield library',
     long_description=long_description,
     long_description_content_type="text/markdown",
